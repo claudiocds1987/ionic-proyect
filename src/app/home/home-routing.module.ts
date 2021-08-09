2ad567ff4edc463adaa1ee313f07bc68f7df162d
claudiocds1987 <cla8787@gmail.com>
2021-08-09 19:27:42 -0300

    add path products/:productId

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component'; // ?
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full'
+  // },
+  {
+    path: 'products/:productId',
+    component: ProductDetailComponent,
   },
 ];
 
