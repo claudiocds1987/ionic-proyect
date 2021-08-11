80b20b2f1cb4111de9255feeb36839c69cc3e9fc
claudiocds1987 <cla8787@gmail.com>
2021-08-11 19:34:26 -0300

    removed productService

--- src/app/home/components/product-detail/product-detail.component.ts
+++ src/app/home/components/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'src/app/services/http-request/product.service';
+import { FakestoreapiService } from '../../../services/http-request/fakestoreapi.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -13,17 +14,22 @@ export class ProductDetailComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private fakeStoreApiService: FakestoreapiService
     ) { }
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
-      const recipeId = paramMap.get('productId');   
-      this.productService.getImage(recipeId).subscribe(res =>{
-        console.log(typeof res);
-        console.log('respuesta get photo ', res);
-        this.product = res;
-      })
+      const recipeId = paramMap.get('productId'); 
+      this.fakeStoreApiService.getProduct(recipeId)
+        .subscribe(res => {
+          this.product = res;
+        })
+      // this.productService.getImage(recipeId).subscribe(res =>{
+      //   console.log(typeof res);
+      //   console.log('respuesta get photo ', res);
+      //   this.product = res;
+      // })
     })
   }
 
