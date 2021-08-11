d7aa6c75236d5a8090d41f640294d4a49378b416
claudiocds1987 <cla8787@gmail.com>
2021-08-11 19:32:39 -0300

    addProduct function

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  private products: any[] = [];
+  // instance cart from BehaviorSubject class
+  public cart = new BehaviorSubject<any[]>([]);
+  // cart$ observer of BehaviorSubject cart
+  public cart$ = this.cart.asObservable();
+
   constructor() { }
+
+  addProduct(product: any){
+    if(localStorage.getItem('shoppingCart') != null){
+      this.products = JSON.parse(localStorage.getItem('shoppingCart'));
+      this.products = [...this.products, product];
+      localStorage.setItem('shoppingCart', JSON.stringify(this.products));
+      // updating BehavoirSubject cart
+      this.cart.next(this.products);
+    }else{
+      // first charge in products array
+      this.products = [];
+      this.products = [...this.products, product];
+      // sending value to BehavoirSubject cart
+      this.cart.next(this.products);
+      // Creating localStorage shoppingCart for first time
+      localStorage.setItem('shoppingCart', JSON.stringify(this.products));
+    }
+  }
+
+
 }
